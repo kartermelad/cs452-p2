963806fab67a81f4e1b2b82e495dec065cca28b4
Karter <132957663+kartermelad@users.noreply.github.com>
2025-02-12 22:57:18 +0000

    starter lab.c setup

---
+++ src/lab.c
@@ -0,0 +1,37 @@
+#include "../src/lab.h"
+
+char *get_prompt(const char *env) {
+
+}
+
+int change_dir(char **dir) {
+
+}
+
+char **cmd_parse(char const *line) {
+
+}
+
+void cmd_free(char ** line) {
+
+}
+
+char *trim_white(char *line) {
+
+}
+
+bool do_builtin(struct shell *sh, char **argv) {
+
+}
+
+void sh_init(struct shell *sh) {
+
+}
+
+void sh_destroy(struct shell *sh) {
+
+}
+
+void parse_args(int argc, char **argv) {
+    
+}
